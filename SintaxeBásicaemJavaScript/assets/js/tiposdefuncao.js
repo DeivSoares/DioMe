1c54e63ec926f1108832cf937140411cc4d2fe44
Deivison Soares <deivisonsoares1610@outlook.com>
2022-09-03 01:50:26 -0300

    Curso Finalizado

---
+++ SintaxeBásicaemJavaScript/assets/js/tiposdefuncao.js
@@ -0,0 +1,25 @@
+// Tipos de Função
+
+//Declarativas
+
+// function funcao(){
+//     console.log('Sou uma mensagem de uma função declarativa')
+// }
+
+// funcao()
+
+//Expressao de Função
+//com/se Nomeação
+
+// var funcao = function(){
+//     console.log('Sou uma mensagem de função de expressão!')
+// }
+
+// funcao()
+
+//Arrow Function
+// var funcao = () =>{
+//     console.log('Sou uma Arrow Function')
+// }
+
+// funcao();
