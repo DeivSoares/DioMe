651b9ee3f6dcd6982372245efe67e3d01fcc0ce6
Deivison Soares <deivisonsoares1610@outlook.com>
2022-09-08 17:06:13 -0300

    .

---
+++ VariáveiseTiposComJavaScript/assets/js/script.js
@@ -0,0 +1,13 @@
+// let obj = {}
+// obj.name = "Deivison"
+// obj.age = 23
+// obj.addres = "Rua Aquilino Borges, Quadra 43/Lote 36 - Rio das Ostras, RJ"
+// console.log(obj)
+
+let person = {
+    name: "Deivison",
+    age: 23,
+    addres: "Rua Aquilino Borges Quadra 43/Lote 36 - Rio das Ostras, RJ"
+}
+
+console.log(person.name)
